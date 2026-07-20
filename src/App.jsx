3bee264297a494574822aeaa26405a61bddb39fb
Ye-1ulx <s25014@gsm.hs.kr>
2026-07-20 14:20:38 +0900

    style: 친구 카드 디자인 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
+// 1. name을 활용해서 개별 클래스명을 추가했습니다!
 function Card({ name, age, hobby }) {
   return (
-    <div className='card'>
+    <div className={`card card-${name}`}> 
       <h2>{name}</h2>
       <p>나이 : {age}살</p>
       <p>취미 : {hobby}</p>
@@ -12,7 +13,6 @@ function Card({ name, age, hobby }) {
 }
 
 function App() {
-
   const friends = [
     { name: '예은', age: 18, hobby: '베이킹' },
     { name: '철수', age: 19, hobby: '축구' },
@@ -22,15 +22,16 @@ function App() {
   return (
     <div>
       <h1>친구 카드 목록</h1>
-      
-      {friends.map((friend) => (
-        <Card 
-          key={friend.name} 
-          name={friend.name} 
-          age={friend.age} 
-          hobby={friend.hobby} 
-        />
-      ))}
+      <div className="card-container">
+        {friends.map((friend) => (
+          <Card 
+            key={friend.name} 
+            name={friend.name} 
+            age={friend.age} 
+            hobby={friend.hobby} 
+          />
+        ))}
+      </div>
     </div>
   )
 }
